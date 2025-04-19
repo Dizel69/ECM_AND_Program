11893acf7e392f4aa0e3195d4edd9edb1e499238
Dizel69 <73982326+Dizel69@users.noreply.github.com>
2025-04-20 02:07:51 +0300

    Create os1.c

---
+++ OperationSystem/Lecture/report2/os1.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <signal.h> 
+#include <stdlib.h>
+void signalhandler(int signal) {
+  printf("caught signal %d, exiting...\n", signal); 
+  exit(1);
+}
+
+int main(void) {
+  signal(SIGINT, signalhandler);
+  while (1);
+  return 0;
+}
